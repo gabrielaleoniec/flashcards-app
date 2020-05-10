cebdd5b902841b667bd40017beed26338c805392
Gabriela Leoniec <gabriela.leoniec@bookassist.com>
2020-05-10 14:06:37 +0200

    The first update and view functions

--- src/Update.js
+++ src/Update.js
@@ -1,5 +1,41 @@
+const MSGS = {
+    'SHOW_FORM' : 'SHOW_FORM',
+    'QUESTION_INPUT' : 'QUESTION_INPUT',
+    'ANSWER_INPUT' : 'ANSWER_INPUT',
+    'SAVE_FORM' : 'SAVE_FORM'
+}
+
+export function showFormMsg(show_form) {
+    return {
+        type: MSGS.SHOW_FORM,
+        show_form
+    }
+}
+
+export function questionInputMsg(question) {
+    return {
+        type: MSGS.QUESTION_INPUT,
+        question
+    }
+}
+
+export function answerInputMsg(answer) {
+    return {
+        type: MSGS.ANSWER_INPUT,
+        answer
+    }
+}
+
+const saveFormMsg = {
+    type: MSGS.SAVE_FORM
+}
+
 function update(model, action) {
     switch (action.type) {
+        case MSGS.SHOW_FORM: {
+            const {show_form} = action;
+            return {...model, show_form}
+        }
         default:
             return model;
     }
--- src/View.js
+++ src/View.js
@@ -1,29 +1,62 @@
 import {h} from 'virtual-dom';
 import hh from 'hyperscript-helpers';
 
-const {div, pre, button, i, h1} = hh(h);
+import {showFormMsg, questionInputMsg, answerInputMsg} from './Update';
 
-function displayFlashCards() {
+const {div, pre, button, i, h1, label, input, textarea} = hh(h);
+
+function displayFlashCards(dispatch, model) {
 
 }
 
-function displayCreateButton(onclick, texts) {
+function displayLabel(labelText, value, onchange) {
+    return label({className: 'db w-100 mv2'}, [
+        labelText,
+        textarea({
+            className: 'pv1 ph2 w-100 mv1',
+            value
+        })
+    ])
+}
+
+function displayForm(dispatch, model) {
+    if(!model.show_form) {
+        return null;
+    }
+    return div({className: 'ba bg-yellow mv3 ph3 pv2 shadow-5'}, [
+        i({
+            className: 'fa fa-close fa-2x fr grow pointer',
+            onclick: e => dispatch(showFormMsg(false))
+        }),
+        displayLabel('Question', model.question, e => dispatch(questionInputMsg(e.target.value))),
+        displayLabel('Answer', model.answer, e => dispatch(answerInputMsg(e.target.value))),
+        button({
+            className: 'pv1 ph3 bg-moon-gray grow pointer f4 white',
+            type: 'button',
+            onclick: e => dispatch(saveFormMsg())
+        }, 'Save')
+    ]);
+}
+
+function displayCreateButton(onclick, text) {
     return button(
         {
-            className: 'pv1 ph3 bg-green f3 white',
+            className: 'pv1 ph3 bg-green grow pointer f3 white',
             type: 'button',
             onclick
         },
-        texts
+        [i({
+            className: 'fa fa-plus mr2'
+        }),
+        text]
     )
 }
 
 function view (dispatch, model) {
     return div({className: 'w-70 center sans-serif'}, [
         h1({className: 'f1 bb'}, 'Flashcard study'),
-        displayCreateButton(e=>console.log(this), [i({
-            className: 'fa fa-plus mr2'
-        }), 'Add Flashcard']),
+        displayCreateButton(e=>dispatch(showFormMsg(true)), 'Add Flashcard'),
+        displayForm(dispatch, model),
         pre(JSON.stringify(model, null, 2))
     ]);
 }
