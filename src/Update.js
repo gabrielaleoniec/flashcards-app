d2eaea16e7b61d952d820cbfe57de11c7282bdfc
Gabriela Leoniec <gabriela.leoniec@bookassist.com>
2020-05-16 12:58:20 +0200

    Showing answear ready

--- src/Update.js
+++ src/Update.js
@@ -6,7 +6,9 @@ const MSGS = {
     'ANSWER_INPUT' : 'ANSWER_INPUT',
     'SAVE_FORM' : 'SAVE_FORM',
     'DELETE_FLASHCARD' : 'DELETE_FLASHCARD',
-    'EDIT_FLASHCARD' : 'EDIT_FLASHCARD'
+    'EDIT_FLASHCARD' : 'EDIT_FLASHCARD',
+    'SHOWHIDE_FLASHCARD' : 'SHOWHIDE_FLASHCARD',
+    'CHANGE_RANK' : 'CHANGE_RANK'
 }
 
 export function showFormMsg(show_form) {
@@ -48,15 +50,33 @@ export function editFlashcardMsg(editId) {
     }
 }
 
+export function changeFlashcardRankMsg(id, rank) {
+    return {
+        type: MSGS.CHANGE_RANK,
+        id,
+        rank
+    }
+}
+
+export function changeFlashcardDisplayMsg(showHideId) {
+    return {
+        type: MSGS.SHOWHIDE_FLASHCARD,
+        showHideId
+    }
+}
+
 function addFlashcard(model) {
     const {next_id, flashcards, question, answear, rank} = model;
-            if (!question || !answear) {
-                return model;
-            }
+    if (!question || !answear) {
+        return model;
+    }
+
     const flashcard = {
         id: next_id,
         question,
         answear,
+        show_answear: false,
+        edit_mode: false,
         rank
     }
     return {...model,
@@ -65,6 +85,23 @@ function addFlashcard(model) {
         answear: '',
         next_id: next_id + 1,
         flashcards: [...flashcards, flashcard]}
+}
+
+function updateFlashcard(model) {
+
+}
+
+function showHideFlashcard(model, id) {
+    const flashcards = model.flashcards.map(
+        f => {
+            if(f.id === id) {
+                const {show_answear} = f;
+                return {...f, show_answear: (!show_answear)}
+            }
+            return f;
+        }
+    );
+    return {...model, flashcards}
 }
 
 function update(model, action) {
@@ -89,9 +126,11 @@ function update(model, action) {
         }
         case MSGS.EDIT_FLASHCARD: {
             const {editId} = action;
-            const {flashcards} = model;
-
-            return {...model, flashcards: updatedFlashcards}
+            return updateFlashcard(model, editId);
+        }
+        case MSGS.SHOWHIDE_FLASHCARD: {
+            const {showHideId} = action;
+            return showHideFlashcard(model, showHideId);
         }
         case MSGS.SAVE_FORM: {
             return addFlashcard(model);
--- src/View.js
+++ src/View.js
@@ -1,58 +1,96 @@
-import {h} from 'virtual-dom';
+import { h } from 'virtual-dom';
 import hh from 'hyperscript-helpers';
 
-import {showFormMsg, questionInputMsg, answerInputMsg, saveFormMsg, deleteFlashcardMsg, editFlashcardMsg} from './Update';
+import {
+    showFormMsg,
+    questionInputMsg,
+    answerInputMsg,
+    saveFormMsg,
+    deleteFlashcardMsg,
+    editFlashcardMsg,
+    changeFlashcardDisplayMsg,
+    changeFlashcardRankMsg
+} from './Update';
 import app from './App';
 
-const {div, pre, button, i, h1, label, textarea, a} = hh(h);
+const { div, pre, button, i, h1, label, textarea, a } = hh(h);
 
-function showAnswear(flashcard) {
-    if(!flashcard.show_answear) {
-        return a('Show answear');
+function displayAnswear(dispatch, flashcard) {
+    if (!flashcard.show_answear) {
+        return a({
+            className: 'underline pointer',
+            onclick: e => dispatch(changeFlashcardDisplayMsg(flashcard.id))
+        }, 'Show answear');
     }
 
     return div(
         [
-            flashcard.answear,
-            a('Show answear')
+            a({
+                className: 'underline pointer',
+                onclick: e => dispatch(changeFlashcardDisplayMsg(flashcard.id))
+            }, 'Hide answear'),
+            div({ className: 'mv2' },
+                flashcard.edit_mode?textarea(flashcard.answear):flashcard.answear),
+            div({ className: 'flex justify-between mv2' }, [
+                answearButton('orange', 'Bad', e => dispatch(changeFlashcardRankMsg(flashcard.id, flashcard.rank))),
+                answearButton('light-purple', 'Good', e => dispatch(changeFlashcardRankMsg(flashcard.id, flashcard.rank + 1))),
+                answearButton('green', 'Great', e => dispatch(changeFlashcardRankMsg(flashcard.id, flashcard.rank + 2)))
+            ])
         ]
     )
 }
 
-function showFlashCard(dispatch, flashcard) {
-    const {id} = flashcard;
+function displayQuestion(dispatch, flashcard) {
+    if(flashcard.edit_mode) {
+        return textarea(flashcard.question);
+    }
+    return flashcard.question;
+}
+
+function answearButton(color, label, onclick) {
+    return button(
+        {
+            className: 'pv1 ba b--dark-green ph3 bg-' + color + ' grow pointer f3 white',
+            onclick
+        },
+        label,
+    );
+}
+
+function displayFlashCard(dispatch, flashcard) {
+    const { id } = flashcard;
     return div({
         className: 'ba bg-yellow mw6 mv3 ph3 pv2 shadow-5'
     },
-    [
-        i({
-            className: 'fa fa-trash fa-2x fr grow pointer',
-            onclick: e => dispatch(deleteFlashcardMsg(id))
-        }),
-        a('Question'),
-        div({
-            className: 'mv2 f3'
-        }, [
-            flashcard.question,
+        [
             i({
-                className: 'fa fa-edit ml2 grow pointer',
-                onclick: e => dispatch(editFlashcardMsg(id))
-            })
-        ]),
-        showAnswear(flashcard)
-    ]
+                className: 'fa fa-trash fa-2x fr grow pointer',
+                onclick: e => dispatch(deleteFlashcardMsg(id))
+            }),
+            a('Question'),
+            div({
+                className: 'mv2 f3'
+            }, [
+                displayQuestion(dispatch, flashcard),
+                i({
+                    className: 'fa fa-edit ml2 grow pointer',
+                    onclick: e => dispatch(editFlashcardMsg(id))
+                })
+            ]),
+            displayAnswear(dispatch, flashcard)
+        ]
     )
 }
 
 function displayFlashCards(dispatch, model) {
-    const {flashcards} = model;
+    const { flashcards } = model;
     return div({},
-        flashcards.map(flashcard => showFlashCard(dispatch, flashcard))
+        flashcards.map(flashcard => displayFlashCard(dispatch, flashcard))
     )
 }
 
 function displayLabel(labelText, value, onchange) {
-    return label({className: 'db w-100 mv2'}, [
+    return label({ className: 'db w-100 mv2' }, [
         labelText,
         textarea({
             className: 'pv1 ph2 w-100 mv1',
@@ -63,10 +101,10 @@ function displayLabel(labelText, value, onchange) {
 }
 
 function displayForm(dispatch, model) {
-    if(!model.show_form) {
+    if (!model.show_form) {
         return null;
     }
-    return div({className: 'ba bg-yellow mw6 mv3 ph3 pv2 shadow-5'}, [
+    return div({ className: 'ba bg-yellow mw6 mv3 ph3 pv2 shadow-5' }, [
         i({
             className: 'fa fa-close fa-2x fr grow pointer',
             onclick: e => dispatch(showFormMsg(false))
@@ -91,14 +129,14 @@ function displayCreateButton(onclick, text) {
         [i({
             className: 'fa fa-plus mr2'
         }),
-        text]
+            text]
     )
 }
 
-function view (dispatch, model) {
-    return div({className: 'w-70 center sans-serif'}, [
-        h1({className: 'f1 bb'}, 'Flashcard study'),
-        displayCreateButton(e=>dispatch(showFormMsg(true)), 'Add Flashcard'),
+function view(dispatch, model) {
+    return div({ className: 'w-70 center sans-serif' }, [
+        h1({ className: 'f1 bb' }, 'Flashcard study'),
+        displayCreateButton(e => dispatch(showFormMsg(true)), 'Add Flashcard'),
         displayForm(dispatch, model),
         displayFlashCards(dispatch, model),
         pre(JSON.stringify(model, null, 2))
